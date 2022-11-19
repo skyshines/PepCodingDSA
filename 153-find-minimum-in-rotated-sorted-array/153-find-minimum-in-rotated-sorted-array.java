50946b5dd699dc2aedf2eb0e2d70a2835ac3594b
Akashdeep singh <87031909+skyshines@users.noreply.github.com>
2022-11-19 22:36:53 +0530

    Time: 1 ms (59.31%), Space: 43.1 MB (10.08%) - LeetHub

--- 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.java
+++ 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.java
@@ -11,15 +11,12 @@ class Solution {
         while(lo <= hi){
             int mid = lo + (hi - lo) / 2;
             
-            if(mid > 0 && arr[mid] < arr[mid - 1]){
-                return arr[mid];
-            }
-            
-            if(mid < arr.length - 1 && arr[mid] > arr[mid +1]){
+            if(arr[mid] > arr[mid +1]){ 
+                /*reason mid + 1upar aya consider test case 5 1 2 3 4 mid - 1bhale exist na kare mid + 1humesha exit karega and because of this we are tesnion free and dont have to check mid + 1 ya mid - 1 exist karega ya nhi*/
                 return arr[mid + 1];
-            }
-            
-            if(arr[lo] <= arr[mid]){
+            }else if(arr[mid] < arr[mid - 1]){
+                return arr[mid];
+            }else if(arr[lo] <= arr[mid]){
                 lo = mid + 1; 
             }else if(arr[mid] <= arr[hi]){
                 hi = mid - 1;
