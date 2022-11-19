ccceca5817ac096f8e91d36c509ff4d553071839
Akashdeep singh <87031909+skyshines@users.noreply.github.com>
2022-11-19 22:39:21 +0530

    Update 153-find-minimum-in-rotated-sorted-array.java

--- 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.java
+++ 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.java
@@ -12,7 +12,10 @@ class Solution {
             int mid = lo + (hi - lo) / 2;
             
             if(arr[mid] > arr[mid +1]){ 
-                /*reason mid + 1upar aya consider test case 5 1 2 3 4 mid - 1bhale exist na kare mid + 1humesha exit karega and because of this we are tesnion free and dont have to check mid + 1 ya mid - 1 exist karega ya nhi*/
+                /*reason mid + 1upar aya consider test case 5 1 2 3 4 mid - 
+                1bhale exist na kare mid + 1humesha exit karega and because of 
+                this we are tesnion free and dont have to check 
+                mid + 1 ya mid - 1 exist karega ya nhi*/
                 return arr[mid + 1];
             }else if(arr[mid] < arr[mid - 1]){
                 return arr[mid];
