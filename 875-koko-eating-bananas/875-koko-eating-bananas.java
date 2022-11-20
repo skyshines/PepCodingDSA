0ba3f6768900f07e2793d2aec5f033e3a29e7522
Akashdeep singh <87031909+skyshines@users.noreply.github.com>
2022-11-20 10:54:23 +0530

    Time: 47 ms (40.76%), Space: 54.1 MB (38.82%) - LeetHub

--- 875-koko-eating-bananas/875-koko-eating-bananas.java
+++ 875-koko-eating-bananas/875-koko-eating-bananas.java
@@ -32,7 +32,9 @@ class Solution {
         int hour = 0;
         
         for(int i = 0; i < arr.length; i++){
-            hour += (int)Math.ceil((double)arr[i] / speed);
+            //hour += (arr[i] + speed - 1) / speed;
+            
+            hour += (int)Math.ceil(arr[i]*1.0 / speed);
             //hour += arr[i] * 1.0 % speed == 0 ? arr[i] * 1.0 / speed : (arr[i] * 1.0/ speed) + 1; time taking
         }
         
@@ -41,6 +43,9 @@ class Solution {
     
     /*
     total += (p+m-1) / m equal to total += Math.ceil( p/m)
+    for new test cases nhi chal rha ye formulae since lo = 1 se start karna chahiye par wo bhi test case par fail ho rha
+    
+    so ideally use ceil function
     
     */
 }
