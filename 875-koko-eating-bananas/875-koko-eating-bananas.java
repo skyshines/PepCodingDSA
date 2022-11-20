c67a6f55499f79c07768ea7fb4dc62278b9681fd
Akashdeep singh <87031909+skyshines@users.noreply.github.com>
2022-11-20 10:11:18 +0530

    Time: 236 ms (5.07%), Space: 53.7 MB (60.12%) - LeetHub

---
+++ 875-koko-eating-bananas/875-koko-eating-bananas.java
@@ -0,0 +1,41 @@
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        int max = Integer.MIN_VALUE;
+        
+        for(int i = 0; i < piles.length; i++){
+            max = (int)Math.max(max,piles[i]);
+        }
+        
+        if(h == piles.length){
+            return max;
+        }
+        
+        int lo = 0; int hi = max; int speed = Integer.MAX_VALUE;
+        
+        while(lo <= hi) {
+            int mid = lo + (hi - lo) / 2; //since idhar array k index nhi balki unki value  concerned hai overflow ho sakta, so to avoid that we write like this
+            
+            if(hours(piles,mid) <= h){ //yadi hours required c kam hai
+//speed ko update kiya because we need minimum speed
+                speed = mid;
+                hi = mid - 1; // to check kahi aur koi minimum speed to exist nhi karti we continued our binary search
+            }else{
+                lo = mid + 1; // hours jada nikle speed badhane padegi
+            }
+            
+        }
+        
+        return speed;
+    }
+    
+    public int hours(int[] arr,int speed){
+        int hour = 0;
+        
+        for(int i = 0; i < arr.length; i++){
+            //hour += (int)Math.ceil(arr[i]*1.0 / speed);
+            hour += arr[i] * 1.0 % speed == 0 ? arr[i] * 1.0 / speed : (arr[i] * 1.0/ speed) + 1; 
+        }
+        
+        return hour;
+    }
+}
