e8327b36b72863cc6c6bdfd5c64c91d5ddc09809
Akashdeep singh <87031909+skyshines@users.noreply.github.com>
2022-11-21 12:57:16 +0530

    Time: 24 ms (50.35%), Space: 40.4 MB (73.01%) - LeetHub

--- 278-first-bad-version/278-first-bad-version.java
+++ 278-first-bad-version/278-first-bad-version.java
@@ -5,19 +5,18 @@ public class Solution extends VersionControl {
     public int firstBadVersion(int n) {
         int lo = 1; 
         int hi = n;
-        int first_bad = 0;
+        //int first_bad = 0;
         
-        while(lo <= hi){
+        while(lo < hi){
             int mid = lo + (hi - lo) / 2;
             
             if(isBadVersion(mid) == true){
-               hi = mid - 1;
-               first_bad = mid; 
+               hi = mid;
             }else{
                 lo = mid + 1;
             }
         }
         
-        return first_bad;
+        return hi;
     }
 }
