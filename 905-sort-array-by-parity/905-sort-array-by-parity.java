774c75dc33b330f5bdbfcfd29cdef72a423d8747
Akashdeep singh <87031909+skyshines@users.noreply.github.com>
2022-11-22 20:33:37 +0530

    Update 905-sort-array-by-parity.java

--- 905-sort-array-by-parity/905-sort-array-by-parity.java
+++ 905-sort-array-by-parity/905-sort-array-by-parity.java
@@ -4,6 +4,12 @@ class Solution {
         int j = nums.length - 1;
         //i plus i k left sare even bande honge
         //j plus j k right sare odd bande honge
+        /*i start hua left c and j right c
+          agar i par even mila sahi hai we did i++
+            agar odd mila phir j ko dekha
+            agar j odd hai bahut badhiya we did j++ and if j is even we swapped them and incremented both
+            we did this till i < j
+            basically array has three parts even unsolved and odd part*/
         while(i < j){
             if(nums[i] % 2 == 0){
                 i++;
