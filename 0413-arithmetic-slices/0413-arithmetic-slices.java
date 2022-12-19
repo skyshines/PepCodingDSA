43da2396a73a2242c341ccb0875b8772df059bdc
Akashdeep singh <87031909+skyshines@users.noreply.github.com>
2022-12-20 00:21:52 +0530

    Time: 0 ms (100.00%), Space: 40.1 MB (91.91%) - LeetHub

--- 0413-arithmetic-slices/0413-arithmetic-slices.java
+++ 0413-arithmetic-slices/0413-arithmetic-slices.java
@@ -4,13 +4,18 @@ class Solution {
             return 0;
         }
         
-        int[] dp = new int[nums.length];
+        //int[] dp = new int[nums.length];
         int sum = 0;
+        int dp1 = 0; int dp2 = 0;
         for(int i = 2; i < nums.length; i++){
             if(nums[i] - nums[i - 1] == nums[i - 1] - nums[i - 2]){
-                dp[i] += dp[i - 1] + 1;
-                sum += dp[i]; 
+                dp2 = dp1 + 1;
+                sum += dp2;
+            }else{
+                dp2 = 0;
             }
+            
+            dp1 = dp2;
         }
         
         return sum;
