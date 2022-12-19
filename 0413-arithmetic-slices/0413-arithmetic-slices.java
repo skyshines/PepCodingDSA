696fb8b564ae9f3ffe3c35db48ee034b9ac08067
Akashdeep singh <87031909+skyshines@users.noreply.github.com>
2022-12-19 23:07:02 +0530

    Time: 1 ms (52.10%), Space: 42.3 MB (21.09%) - LeetHub

--- 0413-arithmetic-slices/0413-arithmetic-slices.java
+++ 0413-arithmetic-slices/0413-arithmetic-slices.java
@@ -4,29 +4,14 @@ class Solution {
             return 0;
         }
         
-        int[] diff = new int[nums.length - 1];
-        
-        for(int i = 0; i < nums.length - 1; i++){
-            diff[i] = nums[i + 1] - nums[i];
-            System.out.print(diff[i] + " ");
-        }
-        System.out.println();
-        
-        int n = 2; int sum = 0;
-        
-        for(int i = 1; i < diff.length; i++){
-            if(diff[i] == diff[i - 1]){
-                
-                n++;
-                
-                System.out.println(n);
-            }else{
-                sum += (n - 2)*(n - 1)/2;
-                n = 2;
+        int[] dp = new int[nums.length];
+        int sum = 0;
+        for(int i = 2; i < nums.length; i++){
+            if(nums[i] - nums[i - 1] == nums[i - 1] - nums[i - 2]){
+                dp[i] += dp[i - 1] + 1;
+                sum += dp[i]; 
             }
         }
-
-        sum += (n - 2)*(n - 1)/2;
         
         return sum;
     }
