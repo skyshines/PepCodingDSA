0477a90d05f1fccfda576e84bd582ee42d9ade7f
Akashdeep singh <87031909+skyshines@users.noreply.github.com>
2022-12-24 00:40:39 +0530

    Time: 3 ms (96.12%), Space: 43.4 MB (75.62%) - LeetHub

--- 0986-interval-list-intersections/0986-interval-list-intersections.java
+++ 0986-interval-list-intersections/0986-interval-list-intersections.java
@@ -1,6 +1,6 @@
 class Solution {
     public int[][] intervalIntersection(int[][] firstList, int[][] secondList) {
-        ArrayList<Integer[]> arr = new ArrayList<>();
+        ArrayList<int[]> arr = new ArrayList<>();
         
         int i = 0; int j = 0;
         
@@ -8,7 +8,7 @@ class Solution {
             int csp = Math.max(firstList[i][0],secondList[j][0]);
             int cep = Math.min(firstList[i][1],secondList[j][1]);
             if(cep >= csp){
-                Integer[] ans = {csp,cep};
+                int[] ans = {csp,cep};
                 arr.add(ans);
                 
             }
@@ -19,14 +19,6 @@ class Solution {
                 }
         }
         
-        int[][] ans = new int[arr.size()][2];
-        
-        for(int k = 0; k < ans.length; k++){
-            for(int l = 0; l < 2; l++){
-                ans[k][l] = arr.get(k)[l];
-            }
-        }
-        
-        return ans;
+        return arr.toArray(new int[arr.size()][2]);
     }
 }
