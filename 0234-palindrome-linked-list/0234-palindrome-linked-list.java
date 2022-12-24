9c0c956737fe4411358267c21ff994e2c8606493
Akashdeep singh <87031909+skyshines@users.noreply.github.com>
2022-12-24 20:57:42 +0530

    Time: 5 ms (88.47%), Space: 52.2 MB (99.22%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -9,25 +9,68 @@
  * }
  */
 class Solution {
-    static ListNode ptr;
-    public boolean solution(ListNode itr){
-        if(itr == null){
-            return true;
+    public ListNode reverse(ListNode head){
+        if(head == null || head.next == null) return head;
+        
+        ListNode curr = head;
+        ListNode prev = null;
+        ListNode frwd = null;
+        
+        while(curr != null){
+            frwd = curr.next;
+            
+            curr.next = prev;
+            
+            prev = curr;
+            curr = frwd;
         }
         
-        boolean flag = solution(itr.next);
-        
-        if(flag == true){
-            boolean ans = itr.val == ptr.val;
-            ptr = ptr.next;
-            return ans;
-        }else{
-            return false;
-        }
+        return prev;
     }
+    
+    public ListNode middle(ListNode head){ // returns middle node of linked list
+        if(head == null || head.next == null) return head;
+        
+        ListNode fast = head;
+        ListNode slow = head;
+        
+        while(fast.next != null && fast.next.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        //for even, first node 
+        return slow;
+    }
+    
+    public boolean solution(ListNode head){
+        ListNode mid = middle(head);
+        ListNode head1 = head;
+        ListNode head2 = mid.next;
+        
+        mid.next = null;
+        
+        head2 = reverse(head2);
+        
+        boolean flag = true;
+        ListNode c2 = head2;
+        ListNode c1 = head1;
+        while(c2 != null){
+            if(c2.val != c1.val){
+                flag = false;
+                break;
+            }
+            
+            c1 = c1.next;
+            c2 = c2.next;
+        }
+        
+        head2 = reverse(head2);
+        mid.next = head2;
+        return flag;
+    }
+    
     public boolean isPalindrome(ListNode head) {
-        if(head.next == null) return true;
-        ptr = head;
+        if(head == null || head.next == null) return true;
         return solution(head);
     }
 }
