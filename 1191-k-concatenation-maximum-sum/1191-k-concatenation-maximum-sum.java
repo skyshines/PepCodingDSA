33e2eb970ec8fccb994fa6cef4c1bc59e87e7290
Akashdeep singh <87031909+skyshines@users.noreply.github.com>
2022-12-24 12:36:28 +0530

    Time: 5 ms (90.14%), Space: 50 MB (84.51%) - LeetHub

--- 1191-k-concatenation-maximum-sum/1191-k-concatenation-maximum-sum.java
+++ 1191-k-concatenation-maximum-sum/1191-k-concatenation-maximum-sum.java
@@ -1,8 +1,8 @@
 class Solution {
-    static long mod = (long)(1e9 + 7);
     
-    public int kadanes(int[] arr, int k){
-        long csum = arr[0]; int osum = arr[0];
+    
+    public long kadanes(int[] arr, int k){
+        long csum = arr[0]; long osum = arr[0];
         
         for(int i = 1; i < arr.length * k; i++){
             if(csum < 0){
@@ -12,7 +12,7 @@ class Solution {
             }
             
             if(csum > osum){
-                osum = (int)csum;
+                osum = csum;
             }
         }
         return osum;
@@ -20,18 +20,18 @@ class Solution {
     
     public int kConcatenationMaxSum(int[] arr, int k) {
         long sum = 0;
-        
+        long mod = (long)(1e9 + 7);
         for(int val : arr) sum += val;
         long val;
         if(k == 1){
-            val = kadanes(arr,1) % mod;
+            val = kadanes(arr,1);
         }else if(sum < 0){
-            val = kadanes(arr,2) % mod;
+            val = kadanes(arr,2);
         }else{
-            val = (long)((kadanes(arr,2) + (k - 2) * sum) % mod);
+            val = kadanes(arr,2) + (k - 2) * sum;
         }
         
         if(val < 0) return 0;
-        else return (int)val;
+        else return (int)(val % mod);
     }
 }
