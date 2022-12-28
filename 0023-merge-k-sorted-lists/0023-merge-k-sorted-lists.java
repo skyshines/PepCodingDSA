e7fb4c1b2f15875ece1679f144be95e2dbb2af8e
Akashdeep singh <87031909+skyshines@users.noreply.github.com>
2022-12-28 17:44:13 +0530

    Time: 1 ms (100.00%), Space: 44.7 MB (69.00%) - LeetHub

---
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -0,0 +1,55 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1,ListNode list2){
+        if(list1 == null || list2 == null) return list1 != null ? list1 : list2;
+        
+        ListNode dummy = new ListNode(0);
+        ListNode prev = dummy;
+        ListNode c1 = list1;
+        ListNode c2 = list2;
+        
+        while(c1 != null && c2 != null){
+            if(c1.val < c2.val){
+                prev.next = c1;
+                c1 = c1.next;
+            }else{
+                prev.next = c2;
+                c2 = c2.next;
+            }
+            
+            prev = prev.next;
+            
+        }
+        
+        prev.next = c1 != null ? c1 : c2;
+        
+        return dummy.next;
+    }
+    
+    public ListNode mergeKLists(ListNode[] lists,int si,int ei){
+        if(si == ei){
+            return lists[si];
+        }
+        int mid = si + (ei - si) / 2;
+        
+        ListNode l1 = mergeKLists(lists,si,mid);
+        ListNode l2 = mergeKLists(lists,mid + 1,ei);
+        
+        return mergeTwoLists(l1,l2);
+    }
+    
+    public ListNode mergeKLists(ListNode[] lists) {
+      if(lists.length == 0)return null;
+       
+       return mergeKLists(lists,0,lists.length - 1); 
+    }
+}
