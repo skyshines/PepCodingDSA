898cdef4de7fbb768db420dd19fd0430e0b4200d
Akashdeep singh <87031909+skyshines@users.noreply.github.com>
2022-12-31 15:38:03 +0530

    Time: 0 ms (100.00%), Space: 40.6 MB (67.15%) - LeetHub

--- 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.java
+++ 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.java
@@ -1,4 +1,30 @@
 class Solution {
+    //linear time and constant space
+    public int minAddToMakeValid(String s) {
+      int opbr = 0;
+      int clbr = 0;
+        
+        for(char ch : s.toCharArray()){
+            if(ch == '('){
+                opbr++;
+            }else{
+                if(opbr > 0){
+                    opbr--;
+                }else{
+                    clbr++;
+                }
+            }
+        }
+        
+        return opbr + clbr;
+        
+        
+    }
+}
+
+/*
+linear time and linear space
+class Solution {
     public int minAddToMakeValid(String s) {
       Stack<Character> st = new Stack<>();
         
@@ -18,3 +44,6 @@ class Solution {
         return st.size();
     }
 }
+
+
+*/
