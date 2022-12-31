47eb2650173af1e5560289d069f6d95d90292618
Akashdeep singh <87031909+skyshines@users.noreply.github.com>
2022-12-31 16:31:11 +0530

    Time: 2 ms (99.74%), Space: 42.4 MB (76.04%) - LeetHub

--- 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.java
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.java
@@ -1,5 +1,29 @@
 class Solution {
     public String removeOuterParentheses(String s) {
+        StringBuilder sb = new StringBuilder("");
+        int opbr = 0;
+        for(char ch : s.toCharArray()){
+            if(ch == '('){
+                if(opbr > 0){
+                    sb.append(ch);
+                }
+                opbr++;
+            }else if(ch == ')'){
+                if(opbr > 1){
+                    sb.append(ch);
+                }
+                opbr--;
+            }
+        }
+        
+        return sb.toString();
+    }
+}
+
+/*
+linear time(traversing string one time and sb to string) and linear space(one stack and one substring)
+class Solution {
+    public String removeOuterParentheses(String s) {
         Stack<Character> st = new Stack<>();
         StringBuilder sb = new StringBuilder("");
         
@@ -21,3 +45,5 @@ class Solution {
         return sb.toString();
     }
 }
+
+*/
