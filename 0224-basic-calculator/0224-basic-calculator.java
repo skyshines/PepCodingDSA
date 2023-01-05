33db9ee8d3ac91811c08fd90ba97c3289b03aa67
Akashdeep singh <87031909+skyshines@users.noreply.github.com>
2023-01-05 21:47:47 +0530

    Time: 10 ms (75.43%), Space: 42.5 MB (79.41%) - LeetHub

---
+++ 0224-basic-calculator/0224-basic-calculator.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int calculate(String s) {
+        int sum = 0;
+        int sign = 1;
+        
+        Stack<Integer> st = new Stack<>();
+        
+        for(int i = 0; i < s.length(); i++){
+            char ch = s.charAt(i);
+            
+            
+                if(ch == '-'){
+                    sign = (-1) * sign;
+                }else if(ch == '('){
+                    st.push(sum);
+                    st.push(sign);
+                    
+                    sum = 0; sign = 1; //reseting sum and sign
+                }else if(ch == ')'){
+                    sum = sum * (st.pop()); // multiplying with sign
+                    sum += st.pop(); // adding the sum
+                }else if(Character.isDigit(ch)){
+                    //extracting the val
+                    int val = 0;
+                    
+                    while(i < s.length() && Character.isDigit(s.charAt(i))){
+                        val = val * 10 + (s.charAt(i) - '0');
+                        i++;
+                    }
+                    
+                    i--;
+                    
+                    val = val * sign;
+                    
+                    sum += val;
+                    sign = 1;
+                }
+            
+        }
+        
+        return sum;
+    }
+}
