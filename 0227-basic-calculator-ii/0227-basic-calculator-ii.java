b9305d85eac51e6bd33555616efde5b37b910385
Akashdeep singh <87031909+skyshines@users.noreply.github.com>
2023-01-05 23:52:36 +0530

    Time: 125 ms (6.73%), Space: 47.5 MB (28.99%) - LeetHub

---
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.java
@@ -0,0 +1,80 @@
+class Solution {
+    public int calculate(String s) {
+        Stack<Character> operators = new Stack<>();
+        Stack<Integer> operands = new Stack<>();
+        
+        for(int i = 0; i < s.length(); i++){
+            char ch = s.charAt(i);
+            
+            if(Character.isDigit(ch) == true){
+                int val = 0;
+                
+                while(i < s.length() && Character.isDigit(s.charAt(i))){
+                    val = val*10 + (s.charAt(i) - '0');
+                    i++;
+                }
+                
+                i--;
+                
+                operands.push(val);
+            }else if(ch == '+' || ch == '*' || ch == '/' || ch == '-'){
+                while(operators.size() > 0 && priority(operators.peek()) >= priority(ch)){
+                    int sec = operands.pop();
+                    int first = operands.pop();
+                    
+                    char op = operators.pop();
+                    
+                    int val = 0;
+                    
+                    if(op == '+'){
+                        val = first + sec;
+                    }else if(op == '-'){
+                        val = first - sec;
+                    }else if(op == '*'){
+                        val = first * sec;
+                    }else if(op == '/'){
+                        val = first / sec;
+                    }
+                    
+                    operands.push(val);
+                }
+                
+                operators.push(ch);
+            }
+            
+        }
+        
+        int val = 0;
+        
+        while(operators.size() > 0){
+            
+            int sec = operands.pop();
+            int first = operands.pop();
+                    
+            char op = operators.pop();
+            
+            if(op == '+'){
+                    val = first + sec;
+            }else if(op == '-'){
+                    val = first - sec;
+            }else if(op == '*'){
+                    val = first * sec;
+            }else if(op == '/'){
+                    val = first / sec;
+            }
+            
+            operands.push(val);
+            
+        }
+        
+        return operands.pop();
+    }
+    
+    public int priority(char ch){
+        if(ch == '+' || ch == '-'){
+            return 0;
+        }else{
+            return 1;
+        }
+    }
+}
