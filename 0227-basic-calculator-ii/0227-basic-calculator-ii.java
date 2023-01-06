15b42b772db0b6a47d18669fce9249f285442388
Akashdeep singh <87031909+skyshines@users.noreply.github.com>
2023-01-06 09:29:46 +0530

    Time: 40 ms (42.96%), Space: 49.2 MB (19.71%) - LeetHub

--- 0227-basic-calculator-ii/0227-basic-calculator-ii.java
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.java
@@ -1,5 +1,46 @@
 class Solution {
     public int calculate(String s) {
+        Stack<Integer> st = new Stack<>();
+        char sign = '+';
+        
+        for(int i = 0; i < s.length(); i++){
+            char ch = s.charAt(i);
+            
+            if(ch == '+' || ch == '/' || ch == '*' || ch == '-'){
+                sign = ch;
+            }else if(Character.isDigit(ch)){
+                int val = 0;
+                
+                while(i < s.length() && Character.isDigit(s.charAt(i))){
+                    val = val * 10 + (s.charAt(i) - '0');
+                    i++;
+                }
+                
+                i--;
+                
+                if(sign == '+' || sign == '-'){
+                    st.push(sign == '+' ? val : (-1) * val);
+                }else if(sign == '*'){
+                    st.push(st.pop() * val);
+                }else if(sign == '/'){
+                    st.push(st.pop() / val);
+                }
+            }
+        }
+        
+        int sum = 0;
+        
+        while(st.size() > 0){
+            sum += st.pop();
+        }
+        
+        return sum;
+    }
+}
+//using two stack one for operators and one for operands
+/*
+class Solution {
+    public int calculate(String s) {
         Stack<Character> operators = new Stack<>();
         Stack<Integer> operands = new Stack<>();
         
@@ -73,3 +114,5 @@ class Solution {
         return val;        
     }
 }
+
+*/
