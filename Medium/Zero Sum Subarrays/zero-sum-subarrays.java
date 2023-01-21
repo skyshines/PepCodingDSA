b4791e24482a1cbd6e64673198c216f801a258b4
Akashdeep singh <87031909+skyshines@users.noreply.github.com>
2023-01-21 18:22:33 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Zero Sum Subarrays/zero-sum-subarrays.java
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+class GFG {
+	public static void main (String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		sc.nextLine();
+		while(t-->0)
+		{
+		    int n;
+		    n=sc.nextInt();
+		    
+		    long arr[]=new long[n];
+	
+		    
+		    for(int i=0;i<n;i++)
+		    {
+		        arr[i]=sc.nextLong();
+		    }
+            Solution ob = new Solution();
+		    System.out.println(ob.findSubarray(arr,n));
+		}
+		
+	}
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    //Function to count subarrays with sum equal to 0.
+    public static long findSubarray(long[] arr ,int n) 
+    {
+        HashMap<Integer, Integer> sumfreq = new HashMap<>();
+        sumfreq.put(0, 1);
+        int count = 0;
+        int sum = 0;
+        
+        for(int i = 0; i < arr.length; i++){
+            sum += arr[i];
+            
+            if(sumfreq.containsKey(sum)){
+                count += sumfreq.get(sum);
+                sumfreq.put(sum, sumfreq.get(sum) + 1);
+            }else{
+                sumfreq.put(sum, 1);
+            }
+        }
+        
+        return count;
+    }
+}
