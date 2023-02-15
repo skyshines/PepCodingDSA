2e8c771dfd08fbb7fcdee7a1daf7c57494be98fa
Akashdeep singh <87031909+skyshines@users.noreply.github.com>
2023-02-16 02:56:19 +0530

    Time: 0 ms (100.00%), Space: 38.8 MB (86.82%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int helper(int n){
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        for(int i = 1; i <= n; i++){
+            dp[i] = dp[i - 1];
+            if(i >= 2) dp[i] += dp[i - 2];
+        }
+        return dp[n];
+    }
+    public int climbStairs(int n) {
+        return helper(n);
+    }
+}
