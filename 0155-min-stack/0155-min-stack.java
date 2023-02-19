a715a615bfd5f969a027b78514b2a7211ed262e2
Akashdeep singh <87031909+skyshines@users.noreply.github.com>
2023-02-19 14:28:29 +0530

    Time: 4 ms (91.92%), Space: 44.4 MB (33.50%) - LeetHub

--- 0155-min-stack/0155-min-stack.java
+++ 0155-min-stack/0155-min-stack.java
@@ -1,4 +1,50 @@
 class MinStack {
+    Stack<Long> data;
+    Long min;
+    
+    public MinStack() {
+        data = new Stack<>();
+    }
+    
+    public void push(int val) {
+        if(data.size() == 0){
+            min = (long)val;
+            data.push(min);
+        }else if((long)val >= min){
+            data.push((long)val);
+        }else{
+            data.push(2*(long)val - min);
+            min = (long)val;
+        }
+    }
+    
+    public void pop() {
+        if(data.peek() >= min){
+            data.pop();
+        }else{
+            long oval = 2*min - data.pop();
+            min = oval;
+        }
+    }
+    
+    public int top() {
+        if(data.peek() >= min){
+            return data.peek().intValue();
+        }else{
+            return min.intValue();
+        }
+    }
+    
+    public int getMin() {
+        return min.intValue();
+    }
+}
+
+/*
+using linear space
+
+/*
+class MinStack {
     Stack<Integer> data;
     Stack<Integer> mindata;
     public MinStack() {
