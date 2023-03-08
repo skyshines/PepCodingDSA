39d22998fabd567b8ce7654ba0addede548781b0
Akashdeep singh <87031909+skyshines@users.noreply.github.com>
2023-03-08 18:12:24 +0530

    Time: 1 ms (100.00%), Space: 43.3 MB (22.66%) - LeetHub

--- 393-utf-8-validation/393-utf-8-validation.java
+++ 393-utf-8-validation/393-utf-8-validation.java
@@ -4,19 +4,19 @@ class Solution {
         
         for(int val : data){
             if(rbtc == 0){
-                if((val >> 7) == 0b0){
+                if(val >> 7 == 0b0){
                     rbtc = 0;
-                }else if((val >> 5) == 0b110){
+                }else if(val >> 5 == 0b110){
                     rbtc = 1;
-                }else if((val >> 4) == 0b1110){
+                }else if(val >> 4 == 0b1110){
                     rbtc = 2;
-                }else if((val >> 3) == 0b11110){
+                }else if(val >> 3 == 0b11110){
                     rbtc = 3;
                 }else{
                     return false;
                 }               
             }else{
-                if((val >> 6) == 0b10){
+                if(val >> 6 == 0b10){
                     rbtc--;
                 }else{
                     return false;
