33bc09c40c5758fbb2f231b762f018a9601fca4b
Akashdeep singh <87031909+skyshines@users.noreply.github.com>
2023-03-16 02:56:05 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Union of Two Sorted Arrays/union-of-two-sorted-arrays.java
@@ -0,0 +1,102 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class Main
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t  =Integer.parseInt(read.readLine());
+        
+        while(t-- > 0)
+        {
+            String st[] = read.readLine().trim().split(" ");
+            int N = Integer.parseInt(st[0]);
+            int M = Integer.parseInt(st[1]);
+            
+            int arr1[] = new int[N];
+            int arr2[] = new int[M];
+            
+            st = read.readLine().trim().split(" ");
+            for(int i = 0; i < N; i++)
+              arr1[i] = Integer.parseInt(st[i]);
+            
+            st = read.readLine().trim().split(" ");  
+            for(int i = 0; i< M; i++)
+              arr2[i] = Integer.parseInt(st[i]);
+            
+            Solution obj = new Solution();
+            ArrayList<Integer> res = new ArrayList<Integer>();
+            res = obj.findUnion(arr1, arr2, N, M);
+            for(int i = 0;i<res.size();i++)
+                System.out.print(res.get(i) + " ");
+            System.out.println(); 
+        }
+    }
+}
+
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+//arr1,arr2 : the arrays
+// n, m: size of arrays
+class Solution
+{
+    //Function to return a list containing the union of the two arrays.
+    public static ArrayList<Integer> findUnion(int arr1[], int arr2[], int n, int m)
+    {
+        ArrayList<Integer> ans = new ArrayList<>();
+        int i = 0;
+        int j = 0;
+        
+        while(i < arr1.length && j < arr2.length){
+            if(arr1[i] == arr2[j]){
+                ans.add(arr1[i]);
+                i++;
+                j++;
+                
+                while(i < arr1.length && arr1[i] == arr1[i - 1]) i++;
+                while(j < arr2.length && arr2[j] == arr2[j - 1]) j++;
+                
+            }else if(arr1[i] < arr2[j]){
+                ans.add(arr1[i]);
+                i++;
+                
+                while(i < arr1.length && arr1[i] == arr1[i - 1]) i++;
+            }else{
+                ans.add(arr2[j]);
+                j++;
+                
+                while(j < arr2.length && arr2[j] == arr2[j - 1]) j++;
+            }
+        }
+        
+        while(i < arr1.length){
+            ans.add(arr1[i]);
+            i++;
+            
+            while(i < arr1.length && arr1[i] == arr1[i - 1]) i++;
+        }
+        
+        while(j < arr2.length){
+            ans.add(arr2[j]);
+            j++;
+            
+            while(j < arr2.length && arr2[j] == arr2[j - 1]) j++;
+        }
+        
+        return ans;
+    }
+}
+
+
+
