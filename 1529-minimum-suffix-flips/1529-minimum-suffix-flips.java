5d0bdb0c4f6fd1d54245d3f85ea279f7d95503bc
Akashdeep singh <87031909+skyshines@users.noreply.github.com>
2023-03-17 14:18:42 +0530

    Time: 6 ms (78.90%), Space: 43.3 MB (14.15%) - LeetHub

--- 1529-minimum-suffix-flips/1529-minimum-suffix-flips.java
+++ 1529-minimum-suffix-flips/1529-minimum-suffix-flips.java
@@ -1,16 +1,17 @@
 class Solution {
     public int minFlips(String target) {
-        char bit = '0';
+        int bit = 0;
         int flips = 0;
         
         for(char ch : target.toCharArray()){
-            if(ch == bit){
+            int x = ch - '0';
+            if(x == bit){
                 
             }else{
+                bit = (bit ^ 1);
                 flips++;
-                if(bit == '0') bit = '1';
-                else bit = '0';
             }
+            
         }
         
         return flips;
