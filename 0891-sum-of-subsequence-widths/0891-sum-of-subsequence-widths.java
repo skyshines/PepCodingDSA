dc3a98dc8a03a8471f29801d1e05ed5ea4ad0032
Akashdeep singh <87031909+skyshines@users.noreply.github.com>
2023-03-18 12:16:54 +0530

    Time: 42 ms (76.83%), Space: 53.6 MB (23.17%) - LeetHub

---
+++ 0891-sum-of-subsequence-widths/0891-sum-of-subsequence-widths.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int sumSubseqWidths(int[] nums) {
+        
+        
+        Arrays.sort(nums);
+        int mod = 1000000007;
+        long ans = 0L;
+        int n = nums.length;
+        
+        //pre calculations
+        long[] pow = new long[nums.length];
+        pow[0] = 1;
+        
+        for(int i = 1; i < nums.length; i++){
+            pow[i] = 2 * (pow[i - 1]) % mod;
+        }
+        
+        
+        for(int i = 0; i < nums.length; i++){
+            ans += (nums[i] * (pow[i] - pow[n - 1 - i]));
+            ans %= mod;
+        }    
+            
+        return (int)(ans);    
+    }
+}
