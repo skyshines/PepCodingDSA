cd18d95db9b845f4edf0bab4cf98d99eac9eec21
Akashdeep singh <87031909+skyshines@users.noreply.github.com>
2023-03-19 22:51:29 +0530

    Time: 1 ms (100.00%), Space: 42.4 MB (100.00%) - LeetHub

---
+++ 2596-check-knight-tour-configuration/2596-check-knight-tour-configuration.java
@@ -0,0 +1,26 @@
+class Solution {
+    int[] iarr = {-1,-2,-2,-1,1,2,2,1};
+    int[] jarr = {-2,-1,1,2,2,1,-1,-2};
+    
+    public boolean helper(int i, int j,int move,int[][] grid){
+        if(i < 0 || j < 0 || i >= grid.length || j >= grid.length || grid[i][j] != move - 1){
+            return false;
+        }
+        
+        if(move == grid.length * grid.length){
+            return true;
+        }
+        
+        for(int k = 7; k >= 0; k--){
+            if(helper(i + iarr[k],j + jarr[k],move + 1,grid)){
+                return true;
+            }
+        }
+        
+        return false;
+    }
+    public boolean checkValidGrid(int[][] grid) {
+        return helper(0,0,1,grid);
+
+    }
+}
