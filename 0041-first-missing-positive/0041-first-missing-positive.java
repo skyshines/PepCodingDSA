731bac7ba283a975803e53ee951d412ea7b8e04b
Akashdeep singh <87031909+skyshines@users.noreply.github.com>
2023-03-19 15:12:41 +0530

    Time: 2 ms (48.81%), Space: 50.6 MB (75.11%) - LeetHub

---
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        boolean oneFound = false;
+        
+        //marking elements out of range
+        for(int i = 0; i < nums.length; i++){
+            
+            if(nums[i] == 1) oneFound = true;
+            
+            if(nums[i] <= 0 || nums[i] > nums.length){
+                nums[i] = 1;
+            }
+        }
+        
+        if(oneFound == false) return 1;
+        
+        //Mapping elements with index
+        
+        for(int i = 0; i < nums.length; i++){
+            int idx = (int)Math.abs(nums[i]) - 1;
+            
+            if(nums[idx] > 0) nums[idx] *= -1;
+        }
+        
+        //Finding Missing Positive
+        
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] > 0){
+                return i + 1;
+            }
+        }
+            
+        return nums.length + 1;    
+    }
+}
