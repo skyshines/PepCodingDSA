cd28c7c91248644aabcf280c0aee13f909ce6ef8
Akashdeep singh <87031909+skyshines@users.noreply.github.com>
2023-03-28 17:02:42 +0530

    Time: 0 ms (100.00%), Space: 42.8 MB (11.45%) - LeetHub

--- 0337-house-robber-iii/0337-house-robber-iii.java
+++ 0337-house-robber-iii/0337-house-robber-iii.java
@@ -13,6 +13,36 @@
  *     }
  * }
  */
+
+class Solution {
+    public int[] rob_(TreeNode node) {
+        if(node == null) return new int[]{0,0};
+        
+        int[] ans = new int[2];
+        
+        int[] left = rob_(node.left);
+        int[] right = rob_(node.right);
+        
+        
+        int withRobbery = node.val + left[1] + right[1];
+        int withoutRobbery = Math.max(left[1], left[0]) + Math.max(right[1], right[0]);
+        
+        ans[0] = withRobbery;
+        ans[1] = withoutRobbery;
+        
+        return ans;
+        
+    }
+    
+    public int rob(TreeNode root) {
+        int[] res = rob_(root);
+        return Math.max(res[0], res[1]);
+    }
+}
+
+/*
+
+//using Class
 class Solution {
     class housePair{
         int withRobbery = 0;
@@ -38,3 +68,5 @@ class Solution {
         return Math.max(res.withRobbery, res.withoutRobbery);
     }
 }
+
+*/
