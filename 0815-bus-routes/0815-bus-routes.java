f727bdb6808573cef8fb3a314088594459c97aea
Akashdeep singh <87031909+skyshines@users.noreply.github.com>
2023-04-10 21:42:37 +0530

    Time: 60 ms (53.60%), Space: 82.5 MB (36.91%) - LeetHub

---
+++ 0815-bus-routes/0815-bus-routes.java
@@ -0,0 +1,60 @@
+class Solution {
+    public int numBusesToDestination(int[][] routes, int source, int target) {
+        HashMap<Integer, ArrayList<Integer>> hm = new HashMap<>();
+        
+        int Bus = 0;
+        
+        for(int[] route : routes){
+            for(int stop : route){
+                ArrayList<Integer> buses = hm.getOrDefault(stop, new ArrayList<>());
+                buses.add(Bus);
+                hm.put(stop, buses);
+            }
+            Bus++;
+        }
+        
+        Queue<Integer> queue = new ArrayDeque<>(); // BFS
+        
+        
+        //visited
+        HashSet<Integer> busVisited = new HashSet<>();
+        HashSet<Integer> busStopVisited = new HashSet<>();
+        
+        queue.add(source);
+        busStopVisited.add(source); //marking visisted and adding in BFS
+        
+        int lev = 0;
+        
+        while(queue.size() > 0){
+            int size = queue.size();
+            
+            for(int i = 0;  i < size; i++)
+            {
+                int busStop = queue.remove();
+                
+                if(busStop == target){
+                    return lev;
+                }else{
+                    ArrayList<Integer> buses = hm.get(busStop);
+                    
+                    for(int bus : buses){
+                        if(busVisited.contains(bus) == true) continue;
+                        
+                        for(int stop : routes[bus]){
+                            if(busStopVisited.contains(stop) == true) continue;
+                            
+                            queue.add(stop);
+                            busStopVisited.add(stop);
+                        }
+                        
+                        busVisited.add(bus);
+                    }
+                }
+            }
+            
+            lev++;
+        }
+        
+        return -1;
+    }
+}
