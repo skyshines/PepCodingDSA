7884101d577e1ecb79b4d5d273eebaf8763325c5
Akashdeep singh <87031909+skyshines@users.noreply.github.com>
2023-04-17 15:09:03 +0530

    Time: 17 ms (28.38%), Space: 43.2 MB (13.40%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.java
+++ 0647-palindromic-substrings/0647-palindromic-substrings.java
@@ -1,25 +1,23 @@
 class Solution {
-    public boolean isP(String s){
-        int i = 0;
-        int j = s.length() - 1;
-        
-        while(i < j){
-            if(s.charAt(i) != s.charAt(j)) return false;
-            i++;
-            j--;
-        }
-        
-        return true;
-    }
     public int countSubstrings(String s) {
+        boolean[][] dp = new boolean[s.length()][s.length()];
         
         int count = 0;
         
-        for(int i = 0; i < s.length(); i++){
-            for(int j = i + 1; j <= s.length(); j++){
-                if(isP(s.substring(i,j)))count++;
+        for(int gap = 0; gap < s.length(); gap++){
+            for(int i = 0, j = gap; j < dp.length; i++, j++){
+                if(gap == 0){
+                    dp[i][j] = true;
+                }else if(gap == 1){
+                    dp[i][j] = s.charAt(i) == s.charAt(j);
+                }else{
+                    dp[i][j] = s.charAt(i) == s.charAt(j) ? dp[i + 1][j - 1] : false;
+                }
+                
+                if(dp[i][j] == true) count++;
             }
         }
+        
         
         return count;
     }
