2b993a5e34c04fa29663d1c51b4f22b1279f259c
Akashdeep singh <87031909+skyshines@users.noreply.github.com>
2023-04-25 17:54:31 +0530

    Time: 0 ms (100.00%), Space: 39.7 MB (32.81%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -9,7 +9,30 @@ class Solution {
         int fibn = fibnm1 + fibnm2;
         return fibn; 
     }
+    
+    public int fibMemoization(int n, int[] dp) {
+       if(n == 0 || n == 1){
+           return n;
+       }
+        
+       if(dp[n] != 0){
+           return dp[n];
+       } 
+        
+        int fibnm1 = fibMemoization(n - 1, dp);
+        int fibnm2 = fibMemoization(n - 2, dp);
+        int fibn = fibnm1 + fibnm2;
+        
+        dp[n] = fibn;
+        
+        return fibn; 
+    }
+    
+    
+    
     public int fib(int n) {
-       return fibRecurrsive(n);
+       //return fibRecurrsive(n);
+       return fibMemoization(n, new int[n + 1]);
+        
     }
 }
