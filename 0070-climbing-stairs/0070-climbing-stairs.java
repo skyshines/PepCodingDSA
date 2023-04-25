86381715b8332562965b6764c362ef0b32e2bb8f
Akashdeep singh <87031909+skyshines@users.noreply.github.com>
2023-04-25 18:25:02 +0530

    Time: 0 ms (100.00%), Space: 39.1 MB (84.79%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,16 +1,24 @@
 class Solution {
-    public int helper(int n){
-        int nm1 = 1;
-        int nm2 = 0;
-        int t = 0;
-        for(int i = 1; i <= n; i++){
-            t = nm1 + nm2;
-            nm2 = nm1;
-            nm1 = t;
+    public int climbStairsMemoization(int n, int[] dp) {
+        if(n == 0){
+            return 1;
+        }else if(n < 0){
+            return 0;
         }
-        return t;
+        
+        if(dp[n] != 0){
+            return dp[n];
+        }
+        
+        int p1 = climbStairsMemoization(n - 1, dp);
+        int p2 = climbStairsMemoization(n - 2, dp);
+        
+        dp[n] = p1 + p2;
+        
+        return dp[n];
     }
-    public int climbStairs(int n) {
-        return helper(n);
+    
+    public int climbStairs(int n){
+        return climbStairsMemoization(n, new int[n + 1]);
     }
 }
