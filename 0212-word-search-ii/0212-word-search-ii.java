76e0e3af256f3996c543eed3523ec800dabc25da
Akashdeep singh <87031909+skyshines@users.noreply.github.com>
2023-05-05 19:02:55 +0530

    Time: 126 ms (84.93%), Space: 42.9 MB (60.90%) - LeetHub

--- 0212-word-search-ii/0212-word-search-ii.java
+++ 0212-word-search-ii/0212-word-search-ii.java
@@ -18,9 +18,9 @@ class Solution {
         for(char ch : word.toCharArray()){
             if(curr.childs[ch - 'a'] == null){
                 curr.childs[ch - 'a'] = new Node();
+                curr.count++;
             }
             
-            curr.count++;
             curr = curr.childs[ch - 'a'];
         }
         
@@ -49,17 +49,20 @@ class Solution {
     int[][] dirs = {{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
     
     public void dfs(char[][] board, int i, int j,Node curr,List<String> ans, boolean[][] visited){
-        if(curr.childs[board[i][j] - 'a'] == null){
+        if(curr.childs[board[i][j] - 'a'] == null || curr.count == 0){
             return;
         }else{
             Node child = curr.childs[board[i][j] - 'a'];
+            
             
             
             if(child.str != null){
                 ans.add(child.str);
                 child.str = null; 
             } 
-            
+            if(child.count == 0){
+                return;
+            }
             //marking visited
             visited[i][j] = true;
             
@@ -68,16 +71,15 @@ class Solution {
                 int rowdash = i + dir[0];
                 int coldash = j + dir[1];
                 
-                if(rowdash < 0 || coldash < 0 || rowdash >= board.length || coldash >= board[0].length || visited[rowdash][coldash] == true || curr.count == 0) continue;
+                if(rowdash < 0 || coldash < 0 || rowdash >= board.length || coldash >= board[0].length || visited[rowdash][coldash] == true) continue;
                 
                 dfs(board, rowdash, coldash, child, ans, visited);
             }
             
             visited[i][j] = false;
             
-            if(child.count == 0){
-                curr.count--;
-            }
+            
+            if(child.count == 0) curr.count--;
         }
     }
 }
