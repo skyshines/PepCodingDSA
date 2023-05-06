568f3ec2aae6f42e633d54299b1c65f3a12376e7
Akashdeep singh <87031909+skyshines@users.noreply.github.com>
2023-05-06 18:43:24 +0530

    Time: 8 ms (23.77%), Space: 43.3 MB (9.90%) - LeetHub

---
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.java
@@ -0,0 +1,39 @@
+class Solution {
+    List<String> ans;
+    
+    public List<String> restoreIpAddresses(String s) {
+        ans = new ArrayList<>();
+        
+        helper(s, 0, 0,"");
+        
+        return ans;
+    }
+    
+    public void helper(String s, int index, int noOfPartitionsSoFar,String asf){
+        if(index == s.length()){
+            if(noOfPartitionsSoFar == 4) ans.add(asf);
+            return;
+        }
+        
+        if(asf.length() != 0){
+            asf = asf + ".";
+        }
+        
+        if(true){
+            helper(s, index + 1, noOfPartitionsSoFar + 1, asf + s.substring(index, index + 1));
+        }
+        
+        if((index + 2 <= s.length()) && s.charAt(index) != '0'){
+            helper(s, index + 2, noOfPartitionsSoFar + 1, asf + s.substring(index, index + 2));
+        }
+        
+        if((index + 3 <= s.length()) && s.charAt(index) != '0'){
+            String add = s.substring(index, index + 3);
+            
+            if(Integer.parseInt(add) <= 255){
+                helper(s, index + 3, noOfPartitionsSoFar + 1, asf + add);
+            }    
+        }
+        
+    }
+}
