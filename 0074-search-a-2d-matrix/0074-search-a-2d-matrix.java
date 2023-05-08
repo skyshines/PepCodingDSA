f5fbeeb668b7d3ca19c3d8e43e00db123ad9887e
Akashdeep singh <87031909+skyshines@users.noreply.github.com>
2023-05-08 17:01:05 +0530

    Time: 0 ms (100.00%), Space: 42.5 MB (25.17%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -2,36 +2,46 @@ class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
         //finding row
         
-        int row = 0;
+        int row = binarySearchRowSelect(matrix, target);
         
+        if(row == -1) return false;
+        
+        return binarySearch(matrix, row, target);
+    }
+    
+    public int binarySearchRowSelect(int[][] matrix, int target){
         int lo = 0;
         int hi = matrix.length - 1;
+        int lc = matrix[0].length - 1;
         
         while(lo <= hi){
             int mid = lo + (hi - lo) / 2;
             
-            if(matrix[mid][0] == target){
-                return true;
-            }else if(matrix[mid][0] > target){
+            if(matrix[mid][0] <= target && target <= matrix[mid][lc]){
+                return mid;
+            }else if(target < matrix[mid][0]){
                 hi = mid - 1;
             }else{
-                row = mid;
                 lo = mid + 1;
             }
         }
         
-        lo = 0;
-        hi = matrix[0].length - 1;
+        return -1;
+    }
+    
+    public boolean binarySearch(int[][] matrix,int row, int target){
+        int lo = 0;
+        int hi = matrix[0].length - 1;
         
         while(lo <= hi){
             int mid = lo + (hi - lo) / 2;
             
             if(matrix[row][mid] == target){
                 return true;
-            }else if(matrix[row][mid] > target){
-                hi = mid - 1;
-            }else{
+            }else if(matrix[row][mid] < target){
                 lo = mid + 1;
+            }else{
+                hi = mid - 1;
             }
         }
         
