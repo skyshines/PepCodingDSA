440a09f581138f15b63dd89414455d8ee0e70f71
Akashdeep singh <87031909+skyshines@users.noreply.github.com>
2023-05-09 12:42:31 +0530

    Time: 4 ms (100.00%), Space: 42.3 MB (92.02%) - LeetHub

--- 0532-k-diff-pairs-in-an-array/0532-k-diff-pairs-in-an-array.java
+++ 0532-k-diff-pairs-in-an-array/0532-k-diff-pairs-in-an-array.java
@@ -16,7 +16,7 @@ class Solution {
                 count++;
                 
                 while(j < arr.length && arr[j] == arr[j - 1]) j++;
-                while(i < j && arr[i] == arr[i - 1]) i++;
+                while(i < arr.length && arr[i] == arr[i - 1]) i++;
                 
             }else if(arr[j] - arr[i] < k){
                 j++;
