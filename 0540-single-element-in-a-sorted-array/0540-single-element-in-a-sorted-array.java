56d294cd60282fdf31071398777f3d257d648f96
Akashdeep singh <87031909+skyshines@users.noreply.github.com>
2023-05-10 21:44:50 +0530

    Time: 0 ms (100.00%), Space: 48.5 MB (43.28%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -1,36 +1,36 @@
 class Solution {
-    public int singleNonDuplicate(int[] nums) {
-        if(nums.length < 3) return nums[0];
-        
+    public int singleNonDuplicate(int[] arr) {
         int lo = 0;
-        int hi = nums.length - 1;
+        int hi = arr.length - 1;
         
-        while(hi - lo + 1 > 3){
-            int mid = (hi + lo) / 2;
-
-            if((mid - lo) % 2 == 0){
-                if(nums[mid] == nums[mid - 1]){
-                    hi = mid;
-                }else if(nums[mid] == nums[mid + 1]){
-                    lo = mid;
-                }else{
-                    return nums[mid];
-                }            
+        while(lo <= hi){
+            if(lo == hi){
+                return arr[lo];
             }else{
-                if(nums[mid] == nums[mid - 1]){
-                    lo = mid + 1;
-                }else{
-                    hi = mid - 1;
+                int mid = lo + (hi - lo) / 2;
+                
+                if(arr[mid] == arr[mid + 1]){
+                    int rightCount = hi - mid + 1;
+                    
+                    if(rightCount % 2 == 1){
+                        lo = mid;
+                    }else{
+                        hi = mid - 1;
+                    }
+                }else if(arr[mid - 1] == arr[mid]){
+                    int leftCount = mid - lo + 1;
+                    
+                    if(leftCount % 2 == 1){
+                        hi = mid;
+                    }else{
+                        lo = mid + 1;
+                    }
+                }else if(arr[mid] != arr[mid + 1] && arr[mid - 1] != arr[mid]){
+                    return arr[mid];
                 }
-            }       
+            }
         }
         
-        int mid = lo + (hi - lo) / 2;
-            
-        if(nums[mid] == nums[mid - 1]){
-            return nums[mid + 1];
-        }else{
-            return nums[mid - 1];
-        }
+        return 0;
     }
 }
