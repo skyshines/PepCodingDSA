a21db364fb892870b38a5e58a768f9c418774b79
Akashdeep singh <87031909+skyshines@users.noreply.github.com>
2023-05-12 01:53:21 +0530

    Added solution - LeetHub

--- Distinct absolute array elements - GFG/distinct-absolute-array-elements.java
+++ Distinct absolute array elements - GFG/distinct-absolute-array-elements.java
@@ -50,16 +50,16 @@ class Solution {
                 i++;
                 j--;
                 
-                while(i <= j && arr[i] == arr[i - 1]) i++;
-                while(i <= j && arr[j] == arr[j + 1]) j--;
+                while(i < arr.length && arr[i] == arr[i - 1]) i++;
+                while(j >= 0 && arr[j] == arr[j + 1]) j--;
             }else if(abs1 > abs2){
                 i++;
                 
-                while(i <= j && arr[i] == arr[i - 1]) i++;
+                while(i < arr.length && arr[i] == arr[i - 1]) i++;
             }else{
                 j--;
                 
-                while(i <= j && arr[j] == arr[j + 1]) j--;
+                while(j >= 0 && arr[j] == arr[j + 1]) j--;
             }
             
             count++;
