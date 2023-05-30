434157383a1a42fda627cfe88f9b66c2b8e881dd
Akashdeep singh <87031909+skyshines@users.noreply.github.com>
2023-05-30 18:05:06 +0530

    Added solution - LeetHub

---
+++ Rod Cutting - GFG/rod-cutting.java
@@ -0,0 +1,75 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class RodCutting {
+
+    public static void main(String args[])throws IOException {
+        BufferedReader in=new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out=new PrintWriter(System.out);
+        
+        int t = Integer.parseInt(in.readLine().trim());
+        while (t-- > 0) {
+            int n = Integer.parseInt(in.readLine().trim());
+            String s[]=in.readLine().trim().split(" ");
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++) arr[i] = Integer.parseInt(s[i]);
+
+            Solution ob = new Solution();
+            out.println(ob.cutRod(arr, n));
+        }
+        out.close();
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution{
+    public void cutStrategy(int[] dp,int[] newPrice){
+        for(int i = 2; i < dp.length; i++){
+            dp[i] = newPrice[i];
+            
+            for(int j = 1; j < i; j++){
+                int potentialAns = newPrice[j] + dp[i - j];
+                
+                if(potentialAns > dp[i]){
+                    dp[i] = potentialAns;
+                }
+            }
+        }
+    }
+    
+    public void leftRightStrategy(int[] dp,int[] newPrice){
+        for(int i = 2; i < dp.length; i++){
+            dp[i] = newPrice[i];
+            
+            int left = 1;
+            int right = i - 1;
+            
+            while(left <= right){
+                if(dp[left] + dp[right] > dp[i]){
+                    dp[i] = dp[left] + dp[right];
+                }
+            }
+        }
+    }
+    
+    public int cutRod(int price[], int n) {
+        //code here
+        int[] newPrice = new int[price.length + 1];
+        
+        for(int i = 0; i < price.length; i++){
+            newPrice[i + 1] = price[i];
+        }
+        
+        int[] dp = new int[price.length + 1];
+        
+        dp[0] = 0;
+        dp[1] = newPrice[1];
+        
+        cutStrategy(dp, newPrice);
+        
+        return dp[dp.length - 1];
+    }
+}
