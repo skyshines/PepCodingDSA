814e40828575ebe7dab67fca8b30449a313e3a0f
Akashdeep singh <87031909+skyshines@users.noreply.github.com>
2023-07-27 01:28:01 +0530

    Time: 18 ms (68.73%), Space: 43.8 MB (61.65%) - LeetHub

--- 1003-check-if-word-is-valid-after-substitutions/1003-check-if-word-is-valid-after-substitutions.java
+++ 1003-check-if-word-is-valid-after-substitutions/1003-check-if-word-is-valid-after-substitutions.java
@@ -3,14 +3,23 @@ class Solution {
         Stack<Character> st = new Stack<>();
         
         for(char ch : s.toCharArray()){
-            if(ch == 'c'){
-                if(st.size() >= 2 && st.pop() == 'b' && st.pop() == 'a'){
+            if(ch == 'a' || ch == 'b'){
+                st.push(ch);
+            }else{
+                char ch1 = '#';
+                char ch2 = '#';
+                if(st.size() >= 2){
+                    ch1 = st.pop();
+                    ch2 = st.pop();
+                }else{
+                    return false;
+                }
+                
+                if(ch1 == 'b' && ch2 == 'a'){
                     
                 }else{
                     return false;
                 }
-            }else{
-                st.push(ch);
             }
         }
         
