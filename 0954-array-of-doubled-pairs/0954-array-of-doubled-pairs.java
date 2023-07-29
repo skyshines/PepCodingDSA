23dca2cfc88ee68dc1a91b04a65c503d31811558
Akashdeep singh <87031909+skyshines@users.noreply.github.com>
2023-07-29 15:00:50 +0530

    Time: 76 ms (33.15%), Space: 48.2 MB (100.00%) - LeetHub

---
+++ 0954-array-of-doubled-pairs/0954-array-of-doubled-pairs.java
@@ -0,0 +1,42 @@
+class Solution {
+    public boolean canReorderDoubled(int[] arr) {
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        
+        for(int val : arr){
+            hm.put(val, hm.getOrDefault(val, 0) + 1);
+        }
+        
+        Integer[] ar = new Integer[arr.length];
+        
+        for(int i = 0; i < arr.length; i++){
+            ar[i] = arr[i];
+        }
+        
+        Arrays.sort(ar, (a,b) -> {
+            return Math.abs(a) - Math.abs(b);
+        });
+        
+        for(int i = 0; i < arr.length; i++){
+            int val = ar[i];
+            
+            if(hm.get(val) == 0) continue;
+            
+            if(hm.getOrDefault(2 * val, 0) == 0){
+                return false;
+            }else{
+                if(val == 0){
+                    if(hm.get(val) >= 2){
+                        hm.put(0, hm.get(0) - 2);
+                    }else{
+                        return false;
+                    }
+                }else{
+                    hm.put( 2 * val, hm.get(2 * val) - 1);
+                    hm.put( val, hm.get(val) - 1);
+                }
+            }
+        }
+        
+        return true;
+    }
+}
