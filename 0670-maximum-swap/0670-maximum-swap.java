f59236e5f781d6ae91547181b9593eba2816046c
Akashdeep singh <87031909+skyshines@users.noreply.github.com>
2023-08-02 01:53:08 +0530

    Time: 1 ms (41.48%), Space: 39.1 MB (85.37%) - LeetHub

---
+++ 0670-maximum-swap/0670-maximum-swap.java
@@ -0,0 +1,37 @@
+class Solution {
+    public void swap(StringBuilder sb, int i, int j){
+        char temp = sb.charAt(i);
+        sb.setCharAt(i, sb.charAt(j));
+        sb.setCharAt(j, temp);
+    }
+    public int maximumSwap(int num) {
+        StringBuilder sb = new StringBuilder(Integer.toString(num));
+        
+        int[] idx = new int[10];
+        
+        for(int i = 0; i < sb.length(); i++){
+            char ch = sb.charAt(i);
+            idx[ch - '0'] = i;
+        }
+        
+        for(int i = 0; i < sb.length(); i++){
+            if(false){
+                
+            }else{
+                boolean flag = true;
+                
+                for(int j = 9; j > sb.charAt(i) - '0'; j--){
+                    if(idx[j] != 0 && idx[j] > i){
+                        flag = false;
+                        swap(sb, i, idx[j]);
+                        break;
+                    }
+                }
+                
+                if(flag == false) break;
+            }
+        }
+        
+        return Integer.parseInt(sb.toString());
+    }
+}
