be21cfce3f1b15c949c5829c06db8e997531e3d3
Akashdeep singh <87031909+skyshines@users.noreply.github.com>
2023-08-02 18:49:55 +0530

    Time: 29 ms (92.76%), Space: 49.9 MB (59.50%) - LeetHub

---
+++ 0632-smallest-range-covering-elements-from-k-lists/0632-smallest-range-covering-elements-from-k-lists.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int[] smallestRange(List<List<Integer>> nums) {
+        int[] res = {-100000, 100000};
+        
+        int k = nums.size();
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> (a[0] - b[0]));
+        
+        int max = Integer.MIN_VALUE;
+        
+        for(int i = 0; i < k; i++){
+            int ele = nums.get(i).get(0);
+            int[] tba = {ele, 0, i};//to be added
+            pq.add(tba);
+            
+            if(ele > max) max = ele;
+        }
+        
+        while(true){
+            int[] rem = pq.remove();
+            
+            if(max - rem[0] + 1 < res[1] - res[0] + 1){
+                res[0] = rem[0];
+                res[1] = max;
+            }
+            
+            if(rem[1] == nums.get(rem[2]).size() - 1){
+                break;
+            }else{
+                int nextEle = nums.get(rem[2]).get(rem[1] + 1);
+                if(nextEle > max) max = nextEle;
+                
+                pq.add(new int[]{nextEle, rem[1] + 1, rem[2]});
+            }
+        }
+        
+        return res;
+    }
+}
