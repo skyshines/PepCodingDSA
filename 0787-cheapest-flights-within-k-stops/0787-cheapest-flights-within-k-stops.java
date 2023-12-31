a5a05662ff46c1e90f466438adae685311324965
Akashdeep singh <87031909+skyshines@users.noreply.github.com>
2024-01-01 02:41:08 +0530

    Time: 5 ms (95.51%), Space: 44.5 MB (19.95%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
@@ -1,24 +1,59 @@
 class Solution {
+    public class Edge{
+        int src;
+        int nbr;
+        int wt;
+        
+        Edge(int src,int nbr,int wt){
+            this.src = src;
+            this.nbr = nbr;
+            this.wt = wt;
+        }
+    }
+    
+    public class Pair{
+        int vtx;
+        int cost;
+        
+        Pair(int vtx, int cost){
+            this.vtx = vtx;
+            this.cost = cost;
+        }
+    }
+    
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
+        ArrayList< Edge>[] graph = new ArrayList[n];
+        for (int i = 0; i < n; i++) {
+            graph[i] = new ArrayList<>();
+        }
+        
+        for(int[] flight : flights){
+            graph[flight[0]].add(new Edge(flight[0],flight[1],flight[2]));
+        }
+        
         int[] path = new int[n];
         Arrays.fill(path, Integer.MAX_VALUE);
         path[src] = 0;
         
-        for(int i = 0; i <= k; i++){
-            int[] temp = Arrays.copyOf(path, n);
-            for(int[] flight : flights){
-                int u = flight[0];
-                int v = flight[1];
-                int wt = flight[2];
+        LinkedList<Pair> queue = new LinkedList<>();
+        queue.add(new Pair(src, 0));
+        k++;
+        
+        
+        while(queue.size() > 0 && k-- > 0){
+            int size = queue.size();
+            
+            while(size-- > 0){
+                Pair rem = queue.remove();
                 
-                if(path[u] == Integer.MAX_VALUE) continue;
-                
-                if(path[u] + wt < temp[v]){
-                    temp[v] = path[u] + wt;
+                for(Edge edge : graph[rem.vtx]){
+                    if(rem.cost + edge.wt < path[edge.nbr]){
+                        path[edge.nbr] = rem.cost + edge.wt;
+                        queue.add(new Pair(edge.nbr, path[edge.nbr]));
+                    }
                 }
             }
             
-            path = temp;
         }
         
         if(path[dst] == Integer.MAX_VALUE){
@@ -28,3 +63,30 @@ class Solution {
         }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
