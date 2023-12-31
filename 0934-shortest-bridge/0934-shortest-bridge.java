2a5633860489a72fac6263f15496a243f82e76ce
Akashdeep singh <87031909+skyshines@users.noreply.github.com>
2023-12-31 16:16:04 +0530

    Time: 9 ms (61.19%), Space: 45.5 MB (5.96%) - LeetHub

--- 0934-shortest-bridge/0934-shortest-bridge.java
+++ 0934-shortest-bridge/0934-shortest-bridge.java
@@ -65,82 +65,80 @@ class Solution {
         return 1;
     }
 }
-/*
-using Class for interview purpose
-class Solution {
-    int[][] dirs = {{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
+
+//using Class for interview purpose
+// class Solution {
+//     int[][] dirs = {{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
     
-    public class Pair{
-        int row;
-        int col;
+//     public class Pair{
+//         int row;
+//         int col;
         
-        Pair(int row, int col){
-            this.row = row;
-            this.col = col;
-        }
-    }
+//         Pair(int row, int col){
+//             this.row = row;
+//             this.col = col;
+//         }
+//     }
     
-    public void dfs(int[][] grid, int row, int col,Queue<Pair> queue){
-        grid[row][col] = -1; // visisted
-        queue.add(new Pair(row, col));
+//     public void dfs(int[][] grid, int row, int col,Queue<Pair> queue){
+//         grid[row][col] = -1; // visisted
+//         queue.add(new Pair(row, col));
         
-        for(int i = 0; i < 4; i++){
-            int rowdash = row + dirs[i][0];
-            int coldash = col + dirs[i][1];
+//         for(int i = 0; i < 4; i++){
+//             int rowdash = row + dirs[i][0];
+//             int coldash = col + dirs[i][1];
             
-            if(rowdash < 0 || coldash < 0 || rowdash >= grid.length || coldash >= grid[0].length || grid[rowdash][coldash] != 1){
-                continue;
-            }
+//             if(rowdash < 0 || coldash < 0 || rowdash >= grid.length || coldash >= grid[0].length || grid[rowdash][coldash] != 1){
+//                 continue;
+//             }
             
-            dfs(grid, rowdash, coldash, queue);
-        }
+//             dfs(grid, rowdash, coldash, queue);
+//         }
         
-    }
+//     }
     
-    public int shortestBridge(int[][] grid) {
-        Queue<Pair> queue = new ArrayDeque<>();
-        boolean flag = false;
+//     public int shortestBridge(int[][] grid) {
+//         Queue<Pair> queue = new ArrayDeque<>();
+//         boolean flag = false;
         
-        for(int i = 0; i < grid.length && flag == false; i++){
-            for(int j = 0; j < grid[0].length && flag == false; j++){
-                if(grid[i][j] == 1){
-                    dfs(grid, i , j, queue);
-                    flag = true;
-                }
-            }
-        }
+//         for(int i = 0; i < grid.length && flag == false; i++){
+//             for(int j = 0; j < grid[0].length && flag == false; j++){
+//                 if(grid[i][j] == 1){
+//                     dfs(grid, i , j, queue);
+//                     flag = true;
+//                 }
+//             }
+//         }
         
-        int lev = 0;
+//         int lev = 0;
         
-        while(queue.size() > 0){
-            int size = queue.size();
+//         while(queue.size() > 0){
+//             int size = queue.size();
             
-            for(int j = 0; j < size; j++){
-                Pair p = queue.remove();
+//             for(int j = 0; j < size; j++){
+//                 Pair p = queue.remove();
                 
-                grid[p.row][p.col] = -1; //visited
+//                 grid[p.row][p.col] = -1; //visited
                 
-                for(int i = 0; i < 4; i++){
-                    int rowdash = p.row + dirs[i][0];
-                    int coldash = p.col + dirs[i][1];
+//                 for(int i = 0; i < 4; i++){
+//                     int rowdash = p.row + dirs[i][0];
+//                     int coldash = p.col + dirs[i][1];
 
-                    if(rowdash < 0 || coldash < 0 || rowdash >= grid.length || coldash >= grid[0].length || grid[rowdash][coldash] == -1){
-                        continue;
-                    }
+//                     if(rowdash < 0 || coldash < 0 || rowdash >= grid.length || coldash >= grid[0].length || grid[rowdash][coldash] == -1){
+//                         continue;
+//                     }
 
-                    if(grid[rowdash][coldash] == 0){
-                        queue.add(new Pair(rowdash, coldash));
-                    }else if(grid[rowdash][coldash] == 1){
-                        return lev;
-                    }
-                }
-            }
+//                     if(grid[rowdash][coldash] == 0){
+//                         queue.add(new Pair(rowdash, coldash));
+//                     }else if(grid[rowdash][coldash] == 1){
+//                         return lev;
+//                     }
+//                 }
+//             }
             
-            lev++;
-        }
+//             lev++;
+//         }
         
-        return 1;
-    }
-}
-
-*/
+//         return 1;
+//     }
+// }
