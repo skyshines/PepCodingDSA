1f4d1bbbb0fb822cbe22bc658ccec0af28499e04
Akashdeep singh <87031909+skyshines@users.noreply.github.com>
2024-01-02 15:29:37 +0530

    Time: 4 ms (7.09%), Space: 44.1 MB (16.47%) - LeetHub

--- 0946-validate-stack-sequences/0946-validate-stack-sequences.java
+++ 0946-validate-stack-sequences/0946-validate-stack-sequences.java
@@ -6,12 +6,14 @@ class Solution {
         int i = 0;
         int j = 0;
         
+        st.push(pushed[i++]);
+        
         while(i < n){
-            st.push(pushed[i++]);
-            
-            while(st.size() > 0 && st.peek() == popped[j]){
+            if(st.size() > 0 && st.peek() == popped[j]){
                 st.pop();
                 j++;
+            }else{
+                st.push(pushed[i++]);
             }
         }
         
