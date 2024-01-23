21f06cc297cb1f220cae5727ec19fe0dd7b5c718
Akashdeep singh <87031909+skyshines@users.noreply.github.com>
2024-01-24 03:51:38 +0530

    Time: 2 ms (46.00%), Space: 41.7 MB (26.16%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.java
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.java
@@ -28,11 +28,39 @@ class Solution {
         
         return ans;
     }
+    
+    public int tabulation(int[] days,int[] costs){
+        int[] dp = new int[400];
+        
+        for(int cday = 399; cday >= 0; cday--){
+            if(cday > days[days.length - 1]){
+                //base case wala continue hoga
+                continue;
+            }
+        
+            int ans = (int)1e8;
+            
+            //we dont need willITravelToday function as we used it to reduce useless calls\
+            if(willITravelToday(cday, days)){
+                ans = dp[cday + 1] + costs[0]; //helper(cday + 1,days,costs,dp) + costs[0];
+
+                ans = Math.min(ans, dp[cday + 7] + costs[1]);//Math.min(ans, helper(cday + 7,days, costs, dp) + costs[1]);
+
+                ans = Math.min(ans, dp[cday + 30] + costs[2]);//Math.min(ans, helper(cday + 30,days,costs, dp) + costs[2]);
+
+                dp[cday] = ans;
+            }else{
+                dp[cday] = dp[cday + 1];
+            }
+       
+        }
+        
+        return dp[days[0]];
+    }
     public int mincostTickets(int[] days, int[] cost) {
         //memoization
         //since only cday varying it can max go to 365 days
         //but since ticket can be purch
-        Integer[] dp = new Integer[366];
-        return helper(days[0], days, cost, dp);
+        return tabulation(days, cost);
     }
 }
