8a2492557392c1c1c16dfad3e1219f8f7b524bb5
Akashdeep singh <87031909+skyshines@users.noreply.github.com>
2024-01-24 03:32:00 +0530

    Time: 1 ms (80.09%), Space: 41.7 MB (26.16%) - LeetHub

---
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.java
@@ -0,0 +1,38 @@
+class Solution {
+    public boolean willITravelToday(int cday,int[] days){
+        for(int day : days){
+            if(day == cday){
+                return true;
+            }
+        }
+        
+        return false;
+    }
+    public int helper(int cday,int[] days,int[] costs,Integer[] dp){
+        if(cday > days[days.length - 1]) return 0;
+        
+        if(dp[cday] != null) return dp[cday];
+        
+        int ans = (int)1e8;
+        if(willITravelToday(cday, days)){
+            ans = helper(cday + 1,days,costs,dp) + costs[0];
+            
+            ans = Math.min(ans, helper(cday + 7,days, costs, dp) + costs[1]);
+            
+            ans = Math.min(ans, helper(cday + 30,days,costs, dp) + costs[2]);
+        }else{
+            ans = helper(cday + 1,days,costs, dp);
+        }
+        
+        dp[cday] = ans;
+        
+        return ans;
+    }
+    public int mincostTickets(int[] days, int[] cost) {
+        //memoization
+        //since only cday varying it can max go to 365 days
+        //but since ticket can be purch
+        Integer[] dp = new Integer[400];
+        return helper(days[0], days, cost, dp);
+    }
+}
